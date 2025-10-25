215650076fea7145aa44136c074e677b6653f3db
ShubhamGupta2701 <shubham272101@gmail.com>
2025-10-25 14:37:08 +0530

    day 1 code

---
+++ conditional_statements.py
@@ -0,0 +1,34 @@
+# what is conditional statements
+
+'''
+when ever we have a condition like - if its true or false
+
+Is num1 is greater than num2 ?
+'''
+
+# >, <, ==, <=, >=
+
+
+# if else
+
+'''
+syntax of if else
+
+if condition:
+    print("true")
+else:
+    print("false)
+'''
+
+# if 9>16: 
+#     print("true")
+# else:
+#     print("inside else")
+#     print("false")
+
+
+name = "shubham"
+if name == "shubham":
+    print("inside if block")
+else:
+    print("inside else block")
--- start.py
+++ start.py
@@ -1,2 +1,21 @@
 print("hi there!")
+print("Python is a Case - Sensitive language")
+
+print(2+3-1)  #Expression
+  
+# this is a single line comment
+
+'''
+this is multi line comment
+hcfowdh
+weifjhiowe
+fjwoiejf]
+fjkhwieouf'fjwheiuf
+cnwioe
+'kjcfwo
+'''
+
+num = 9.876
+
+print(type(num))
 
