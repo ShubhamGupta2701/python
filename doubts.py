3bbc4930db40fcc735042068655744613c1039b6
ShubhamGupta2701 <shubham272101@gmail.com>
2025-10-27 20:49:39 +0530

    adding files for day2

---
+++ doubts.py
@@ -0,0 +1,25 @@
+# ( 2 * 5 > 9 ) : why type should be a Boolean (Bool)
+
+# print( 2 * 5 > 9 )
+
+# 2*5 = 10 
+# 10 > 9
+
+
+name = "kawal"   # single '=' is used to assign a value to a variable
+name = "shubham"
+if name == "shubham":
+    print(name)
+
+# print(name = "shubham") # double '==' is used to compare values
+
+# print(name = 'kawal')
+
+
+# >, <,>=, <=, ==   - condiotional operators
+
+number = 101
+number = number - 100
+
+number = 101 - 100 = 1
+number = 1
